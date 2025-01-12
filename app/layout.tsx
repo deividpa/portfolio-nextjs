0080360bcab8fb98dada5ee2ef0708e38d89b00a
David Pérez <deividpa022@gmail.com>
2025-01-11 19:49:19 -0500

    feat: Remove unused font import and simplify body class in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "David Perez - Software Engineer - Portfolio",
@@ -20,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} antialiased`}>
+      <body className={`antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
